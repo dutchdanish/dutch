427c6f44960a514e1887e73182c9d2b72453361f
Ubuntu <ubuntu@ip-172-31-63-228.ec2.internal>
2016-03-11 18:50:41 +0000

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome_page/welcome'
+ get 'welcome_page/welcome'
 root 'welcome_page#welcome'
 
+resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
