482d365fdf00e830e35ea3e538bf371894b56043
Ubuntu <ubuntu@ip-172-31-63-228.ec2.internal>
2016-03-12 02:16:25 +0000

    a

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+def new
+end
 
  def create
    @article = Article.new(article_params)
@@ -12,6 +14,8 @@ private
     params.require(:article).permit(:title, :text)
 
  end
+end
+class ArticlesController < ApplicationController
 
 def show 
 @article = Article.find(params[:id])
