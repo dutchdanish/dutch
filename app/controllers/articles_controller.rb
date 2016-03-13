e40a49c50e8c0be9f71885f16e533d5523c2df1d
Ubuntu <ubuntu@ip-172-31-63-228.ec2.internal>
2016-03-13 17:23:13 +0000

    asdf

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,3 +28,17 @@ class ArticlesController < ApplicationController
  end
 
 end
+
+class ArticlesController < ApplicationController
+ def index
+ @articles = Article.all
+ end
+
+ def show
+ @article = Article.find(params[:id])
+ end
+
+ def new
+ end
+end
+
